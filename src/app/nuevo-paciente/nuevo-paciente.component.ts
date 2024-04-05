f13900faf190fc616ce6d1c58d737cd902c71733
njorquera98 <njorquera98.dev@gmail.com>
2024-04-04 21:50:19 -0300

    Nuevas paginas
    Pagina de inicio de sesion y nuevo paciente, falta agregar el routing entre paginas

---
+++ src/app/nuevo-paciente/nuevo-paciente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-paciente',
+  standalone: true,
+  imports: [],
+  templateUrl: './nuevo-paciente.component.html',
+  styleUrl: './nuevo-paciente.component.css'
+})
+export class NuevoPacienteComponent {
+
+}
