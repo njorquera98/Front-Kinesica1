eb4a4c580a40f9498514d59f603d6fce6fae7a89
njorquera98 <njorquera98.dev@gmail.com>
2024-04-10 13:29:48 -0400

    Add: Lista Pacientes
    Lista creada con los campos y los botones a usar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { NuevoPacienteComponent } from './nuevo-paciente/nuevo-paciente.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { ListaPacientesComponent } from './lista-pacientes/lista-pacientes.component';
 
 export const routes: Routes = [
   { path: 'login', component: LogInComponent },
+  { path: 'lista', component: ListaPacientesComponent },
   { path: 'paciente', component: NuevoPacienteComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
---
+++ src/app/lista-pacientes/lista-pacientes.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+
+export interface PeriodicElement {
+  nombre: string;
+  apellido: string;
+  telefono: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { nombre: 'Paloma', apellido: 'Alvarez', telefono: 974547947 },
+  { nombre: 'Ignacio', apellido: 'Ancoma', telefono: 975217404 },
+  { nombre: 'Valentina', apellido: 'Aravena', telefono: 977605142 },
+  { nombre: 'Matias', apellido: 'Arenas', telefono: 963007630 },
+  { nombre: 'Williams', apellido: 'Astudillos', telefono: 941010715 },
+  { nombre: 'Maria', apellido: 'Aviles', telefono: 972906408 },
+  { nombre: 'Jose', apellido: 'Balcarse', telefono: 961749470 },
+  { nombre: 'Fernanda', apellido: 'Becerra', telefono: 991255542 },
+  { nombre: 'Alvaro', apellido: 'Bernal', telefono: 964188311 },
+  { nombre: 'Pamela', apellido: 'Bonilla', telefono: 939699747 },
+];
+
+@Component({
+  selector: 'app-lista-pacientes',
+  standalone: true,
+  imports: [
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  templateUrl: './lista-pacientes.component.html',
+  styleUrl: './lista-pacientes.component.css'
+})
+export class ListaPacientesComponent {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+}
